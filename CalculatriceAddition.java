1b349a585121faec57ceea4783c719e612697107
hajarol18 <hajaroulabasse2003@gmail.com>
2024-10-31 21:57:32 +0100

    Update CalculatriceAddition.java

--- CalculatriceAddition.java
+++ CalculatriceAddition.java
@@ -1,8 +1,8 @@
 public class CalculatriceAddition {
     public static void main(String[] args) {
-        int nombre1 = 5; 
-        int nombre2 = 7; 
-        int somme = nombre1 + nombre2;
+        double nombre1 = 5; // Remplace 5 par le premier nombre
+        double nombre2 = 7; // Remplace 7 par le deuxième nombre
+        double somme = nombre1 + nombre2;
         System.out.println("La somme des deux nombres est : " + somme);
     }
 }
